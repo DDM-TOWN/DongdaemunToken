454a6e00f5cfb1fb84381dad636117a4c63d56c8
DDM-TOWN <111842991+DDM-TOWN@users.noreply.github.com>
2023-06-30 17:16:20 +0900

    change to 1e9

--- DDMToken.sol
+++ DDMToken.sol
@@ -7,7 +7,7 @@ import "./Context.sol";
 import "./ERC20Burnable.sol";
 
 /**
- * @title Dong Dae Moon Token
+ * @title Dongdaemun Token
  * @dev ERC20 Token, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
@@ -18,7 +18,7 @@ contract DDMToken is Context, ERC20, ERC20Burnable {
      * @dev Constructor that gives _msgSender() all of existing tokens.
      */
     constructor () ERC20("Dongdaemun Token", "DDMT") {
-        _mint(_msgSender(), 1000000000 * (10 ** uint256(decimals())));
+        _mint(_msgSender(), 1e9 * (10 ** uint256(decimals())));
     }
 
 
